c8bb5b72c03ef70e7e5b5df36bb661d1d94a8068
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2021-08-19 19:40:16 -0400

    adding javascript

--- js/site.js
+++ js/site.js
@@ -1,11 +1,27 @@
 function getValue() {
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let reversedString = reverseString(userString);
+
+    displayString(reversedString);
+}
+
+function reverseString(userString) {
+    let reversedString = [];
+
+    for (let i = userString.length - 1; i >= 0; i--) {
+        reversedString += userString[i];
+    }
+  
+    return reversedString;
 
 }
 
-function reverseString() {
+function displayString(reversedString) {
+    document.getElementById("message").innerHTML = `Your string reveresed is: ${reversedString}`;
 
-}
+    document.getElementById("alert").classList.remove("invisible");
 
-function displayString() {
-    
 }
