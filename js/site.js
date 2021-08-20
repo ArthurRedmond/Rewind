4a34d6071c822c6f14e6bddfdeeca9a739a9de04
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2021-08-20 19:23:32 -0400

    Update code.html

--- js/site.js
+++ js/site.js
@@ -1,27 +1,19 @@
 function getValue() {
     document.getElementById("alert").classList.add("invisible");
-
     let userString = document.getElementById("userString").value;
-
     let reversedString = reverseString(userString);
-
     displayString(reversedString);
 }
 
 function reverseString(userString) {
-    let reversedString = [];
-
+    let reversedString = "";
     for (let i = userString.length - 1; i >= 0; i--) {
         reversedString += userString[i];
     }
-  
     return reversedString;
-
 }
 
 function displayString(reversedString) {
     document.getElementById("message").innerHTML = `Your string reveresed is: ${reversedString}`;
-
     document.getElementById("alert").classList.remove("invisible");
-
 }
